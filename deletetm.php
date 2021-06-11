9e9ac54b80973959bd3ba8cde6d9e7eb351e6aa2
Lalu07 <63830491+Lalu07@users.noreply.github.com>
2021-06-11 11:14:55 +0800

    Add files via upload

--- deletetm.php
+++ deletetm.php
@@ -6,29 +6,24 @@
 
 	$conn = mysqli_connect($server, $user, $password, $namadb) or die("Koneksi gagal");
 
+	$id		= $_POST['id'];
 	$nama 	= $_POST['nama'];
 	$telpon	= $_POST['telpon'];
 
 	class emp{}
+		$query = mysqli_query($conn,"DELETE from datatemanbaru WHERE id = '".$id."' ");
 
-	if (empty($nama) || empty($telpon)) {
-		
-		$response = new emp();
-		$response->success = 0;
-		$response->message = "Data tidak boleh kosong";
-		die(json_encode($response));
-	}else{
-		$query = mysqli_query($conn,"INSERT INTO datatemanbaru (nama,telpon) values ('$nama','$telpon')");
 		if ($query) {
+			
 			$response = new emp();
 			$response->success = 1;
-			$response->message = "Data Berhasil Disimpan";
+			$response->message = "Data berhasil dihapus";
 			die(json_encode($response));
+
 		}else{
 			$response = new emp();
-			$response->success = 0;
-			$response-> message = "Gagal simpan Data";
+			$response -> success = 0;
+			$response -> message = "Gagal menghapus data";
 			die(json_encode($response));
 		}
-	}
 ?>
